60566b3c635973c6eba20fc94f9ea5f24574e54c
Charles Stover <github@charlesstover.com>
2019-05-23 20:27:52 -0700

    reactn-devtools

--- src/charlesstover/index.js
+++ src/charlesstover/index.js
@@ -116,6 +116,9 @@ module.exports = {
     media: 'reactn.png',
     status: `ReactN is a extension of #ReactJS that includes global state management. ${github}reactn ${hashtags('ReactJS', 'frontend', 'react', 'typescript')}`,
   },
+  'reactn-devtools': {
+    status: `ReactN DevTools allow you to view your ReactN global state with your browser's own Redux DevTools extension. ${hashtags(null, 'frontend', 'reactj', 'reactjs', 'redux', 'typescript')}`,
+  },
   'rn-webview': {
     status: `The rn-webview package fixes #ReactNative’s postMessage for iOS. ${github}react-native-web-view ${hashtags('ReactNative', 'reactnative')}`,
   },
